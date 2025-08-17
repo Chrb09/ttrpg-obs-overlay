1acadb2450bc04d10419213bf50dd8a0aa18638d
Chrb09 <carloshrbarile@gmail.com>
2025-08-17 18:28:29 -0300

    remake do layout de mythic bastionland

--- pages/overlay/[id]/[charId].tsx
+++ pages/overlay/[id]/[charId].tsx
@@ -72,7 +72,7 @@ function OverlayCharacter() {
                 <img
                   src={character.icon}
                   alt={character.name}
-                  className="w-[12em] absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] select-none"
+                  className="w-[12em] absolute left-[50%] top-[42%] translate-y-[-50%] translate-x-[-50%] select-none"
                 />
                 {fatigadoStat?.value == true && (
                   <img
@@ -85,56 +85,60 @@ function OverlayCharacter() {
                   className="w-[20em] absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] select-none"
                 />
                 <img
+                  src="/overlays/mythic/luz.png"
+                  className="w-[20em] absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50.5%] select-none"
+                />
+                <img
                   src="/overlays/mythic/sombra.png"
                   className="w-[20em] mix-blend-multiply absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] select-none"
                 />
                 <img
-                  src="/overlays/mythic/line2.png"
+                  src="/overlays/mythic/line.png"
                   className="w-[20em] absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] select-none"
                 />
                 <div
-                  className={`absolute left-[50%] top-[62.5%] translate-y-[-50%] translate-x-[-50%] text-[1.5em] ${manufacturingConsent.className}`}>
+                  className={`absolute left-[50%] top-[56.5%] translate-y-[-50%] translate-x-[-50%] text-[1.85em] ${manufacturingConsent.className}`}>
                   {character.name}
                 </div>
-                <div className="absolute left-[50%] top-[22%] font-bold text-[1.75em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[50%] top-[17%] font-bold text-[1.75em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {vigorStat?.value}
                 </div>
-                <div className="absolute left-[50%] top-[30.75%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[50%] top-[25.25%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {vigorStat?.max}
                 </div>
-                <div className="absolute left-[24.5%] top-[30.95%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[24.5%] top-[25.5%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {clarezaStat?.value}
                 </div>
-                <div className="absolute left-[18%] top-[34.5%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[18%] top-[29%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {clarezaStat?.max}
                 </div>
-                <div className="absolute right-[24.5%] top-[30.95%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[50%] text-center">
+                <div className="absolute right-[24.5%] top-[25.5%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[50%] text-center">
                   {espiritoStat?.value}
                 </div>
-                <div className="absolute right-[18%] top-[34.5%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[50%] text-center">
+                <div className="absolute right-[18%] top-[29%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[50%] text-center">
                   {espiritoStat?.max}
                 </div>
-                <div className="absolute left-[50%] top-[72.4%] font-bold text-[1.85em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[50%] top-[70%] font-bold text-[1.85em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {gloriaStat?.value}
                 </div>
-                <div className="absolute left-[22.5%] top-[71.2%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[24.5%] top-[65.5%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {guardaStat?.value}
                 </div>
-                <div className="absolute left-[27.5%] top-[75%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
+                <div className="absolute left-[29.5%] top-[69.5%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[-50%] text-center">
                   {guardaStat?.max}
                 </div>
-                <div className="absolute right-[22.5%] top-[71.2%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[50%] text-center">
+                <div className="absolute right-[25%] top-[65.5%] font-bold text-[1.3em] translate-y-[-50%] translate-x-[50%] text-center">
                   {armaduraStat?.value}
                 </div>
-                <div className="absolute right-[27.5%] top-[75%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[50%] text-center">
+                <div className="absolute right-[30%] top-[69.5%] font-bold text-[0.85em] translate-y-[-50%] translate-x-[50%] text-center">
                   {armaduraStat?.value}
                 </div>
                 <div
-                  className={`absolute left-[20%] top-[77%] font-bold text-[1.2em] translate-y-[-50%] translate-x-[-50%] text-center ${manufacturingConsent.className}`}>
+                  className={`absolute left-[36%] top-[64.5%] font-bold text-[1.2em] translate-y-[-50%] translate-x-[-50%] text-center ${manufacturingConsent.className}`}>
                   GD
                 </div>
                 <div
-                  className={`absolute right-[20%] top-[77%] font-bold text-[1.6em] translate-y-[-50%] translate-x-[50%] text-center ${manufacturingConsent.className}`}>
+                  className={`absolute right-[36%] top-[64%] font-bold text-[1.4em] translate-y-[-50%] translate-x-[50%] text-center ${manufacturingConsent.className}`}>
                   A
                 </div>
               </div>
